16d19c753c24e9a75524b0302afbecb4c88ead35
anangsibian <anangsibian@gmail.com>
2025-09-03 23:28:24 +0700

    Add files via upload

---
+++ DemoNilai.java
@@ -0,0 +1,9 @@
+package DemoNilai;
+
+public class DemoNilai {
+	public static void main(String[] args) {
+        Nilai n = new Nilai();
+        n.Nilai("2410631170142", "Anang", 90, 90, 90, 90);
+        n.CetakNilai();
+    }
+}
